3ab1b3d140d1f35a32e065d2fd498a9f8e905731
Kuba1710 <63730483+Kuba1710@users.noreply.github.com>
2024-10-06 20:21:38 +0200

    second commit

--- makemore.py
+++ makemore.py
@@ -1,6 +1,8 @@
 import torch
 import matplotlib.pyplot as plt
 import os
+import torch.nn.functional as F
+
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 words = open('names.txt', 'r').read().splitlines()
@@ -30,7 +32,7 @@ itos = {i:s for s,i in stoi.items()}
 
 g = torch.Generator().manual_seed(2147483647)
 
-P = N.float()
+P = (N+1).float()
 P /=  P.sum(1, keepdims=True)
 
 for i in range(5):
@@ -48,4 +50,69 @@ for i in range(5):
         if ix == 0:
             break
     
-    print(''.join(out))
+    #print(''.join(out))
+
+log_likelihood = 0.0
+n = 0
+
+for w in words:
+    chs = ['.'] + list(w) + ['.']
+    for ch1, ch2 in zip(chs, chs[1:]):
+        ix1 = stoi[ch1]
+        ix2 = stoi[ch2]
+        prob = P[ix1, ix2]
+        logprob = torch.log(prob)
+        log_likelihood += logprob
+        n += 1
+        #print (f'{ch1}{ch2}: {prob:.4f} {logprob:.4f}')
+    
+#print(f'{log_likelihood =}')
+nll = -log_likelihood
+#print(f'{nll=}')
+#print(f'{nll/n=}')
+
+xs, ys = [], []
+
+for w in words[:1]:
+    chs = ['.'] + list(w) + ['.']
+    for ch1, ch2 in zip(chs, chs[1:]):
+        ix1 = stoi[ch1]
+        ix2 = stoi[ch2]
+        print(ch1, ch2)
+        xs.append(ix1)
+        ys.append(ix2)
+
+xs = torch.tensor(xs)
+ys = torch.tensor(ys)
+
+g = torch.Generator().manual_seed(2147483647)
+W = torch.randn((27, 27), generator=g, requires_grad=True)
+
+for k in range(200):
+    out = []
+    ix = 0
+    while True:
+        xenc = F.one_hot(xs, num_classes=27).float() # zmiana danych w wejsciowych w wektor 0 i 1 
+        #plt.imshow(xenc)
+        #plt.show()
+        #print((xenc @ W)[3,13])
+        #print(xenc[3])
+        #print(W[:, 13])
+        #print((xenc[3] * W[:, 13]).sum())
+        #print((xenc[3] * W[:, 13]))
+
+        logits = xenc @ W #log-counts
+        counts = logits.exp()
+        probs = counts / counts.sum(1, keepdims=True)
+        loss = -probs[torch.arange(5), ys].log().mean()
+        print(loss.item())
+
+        #backward pass
+        W.grad = None #gradient to zero
+        loss.backward()
+
+        W.data += -50 * W.grad
+
+
+
+
