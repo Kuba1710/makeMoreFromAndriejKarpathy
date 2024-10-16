7f256299f012132a53d8e2fb396dfa94571a75fe
Kuba1710 <63730483+Kuba1710@users.noreply.github.com>
2024-10-16 18:45:46 +0200

    batch normalization

--- makemore3.py
+++ makemore3.py
@@ -51,7 +51,13 @@ b1 = torch.randn(n_hidden,                        generator=g) * 0.01
 W2 = torch.randn((n_hidden, vocab_size),          generator=g) * 0.01
 b2 = torch.randn(vocab_size,                      generator=g) * 0
 
-parameters = [C, W1, b1,  W2, b2]
+bngain = torch.ones((1, n_hidden))
+bnbias = torch.zeros((1, n_hidden))
+
+bnmean_running = torch.zeros((1, n_hidden))
+bnstd_running = torch.ones((1, n_hidden))
+
+parameters = [C, W1, b1,  W2, b2, bngain, bnbias]
 print(sum(p.nelement() for p in parameters)) # number of parameters in total
 for p in parameters:
   p.requires_grad = True
@@ -70,7 +76,15 @@ for i in range(max_steps):
   # forward pass
   emb = C[Xb] # embed the characters into vectors
   embcat = emb.view(emb.shape[0], -1) # concatenate the vectors
-  hpreact = embcat @ W1 + b1 # hidden layer pre-activation
+  hpreact = embcat @ W1 #+ b1 # hidden layer pre-activation
+  bnmeani = hpreact.mean(0, keepdim=True)
+  bnstdi = hpreact.std(0, keepdim=True)
+  hpreact = bngain * (hpreact - bnmeani) / bnstdi + bnbias
+
+  with torch.no_grad():
+    bnmean_running = 0.999 * bnmean_running + 0.001 * bnmeani
+    bnstd_running = 0.999 * bnstd_running + 0.001 * bnstdi
+
   h = torch.tanh(hpreact) # hidden layer
   logits = h @ W2 + b2 # output layer
   loss = F.cross_entropy(logits, Yb) # loss function
@@ -90,6 +104,17 @@ for i in range(max_steps):
     print(f'{i:7d}/{max_steps:7d}: {loss.item():.4f}')
   lossi.append(loss.log10().item())
 
+# calibrate the batch norm at the end of training
+
+with torch.no_grad():
+  # pass the training set through
+  emb = C[Xtr]
+  embcat = emb.view(emb.shape[0], -1)
+  hpreact = embcat @ W1 # + b1
+  # measure the mean/std over the entire training set
+  bnmean = hpreact.mean(0, keepdim=True)
+  bnstd = hpreact.std(0, keepdim=True)
+
 @torch.no_grad() # this decorator disables gradient tracking
 def split_loss(split):
   x,y = {
@@ -100,7 +125,7 @@ def split_loss(split):
   emb = C[x] # (N, block_size, n_embd)
   embcat = emb.view(emb.shape[0], -1) # concat into (N, block_size * n_embd)
   hpreact = embcat @ W1 + b1
-  #hpreact = bngain * (hpreact - hpreact.mean(0, keepdim=True)) / hpreact.std(0, keepdim=True) + bnbias
+  hpreact = bngain * (hpreact -bnmean) / bnstd + bnbias
   h = torch.tanh(hpreact) # (N, n_hidden)
   logits = h @ W2 + b2 # (N, vocab_size)
   loss = F.cross_entropy(logits, y)
@@ -119,7 +144,7 @@ for _ in range(20):
       # forward pass the neural net
       emb = C[torch.tensor([context])] # (1,block_size,n_embd)
       h = torch.tanh(emb.view(1, -1) @ W1 +b1)
-      logits = x
+      logits = h
       probs = F.softmax(logits, dim=1)
       # sample from the distribution
       ix = torch.multinomial(probs, num_samples=1, generator=g).item()
